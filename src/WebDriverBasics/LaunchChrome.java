53563937f5ac3653ffc893d5521573da96c563e6
bhimskr <pkbhimskr@gmail.com>
2019-07-13 20:15:44 +0530

    WebDriverBasics

---
+++ src/WebDriverBasics/LaunchChrome.java
@@ -0,0 +1,31 @@
+package WebDriverBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+		String title=driver.getTitle();
+		System.out.println(title);
+		if(title.equals("Google")) {
+			System.out.println("correct title");
+		}
+		else {
+			System.out.println("Incorrect title");
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
